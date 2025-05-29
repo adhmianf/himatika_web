57019d597d271ab9b6f4ea40b1e0cbb6f0acf702
adhmianf <149399871+adhmianf@users.noreply.github.com>
2025-05-29 18:26:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+  <div class="text-center">
+    <h1>Selamat Datang di Website HIMATIKA</h1>
+    <p class="lead">Himpunan Mahasiswa Matematika</p>
+  </div>
+<?php include 'includes/footer.php'; ?>
