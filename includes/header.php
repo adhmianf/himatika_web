f516768678558908e460641fe86bd11ed26d5716
adhmianf <149399871+adhmianf@users.noreply.github.com>
2025-05-29 18:30:53 +0700

    header.php

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Profil HIMATIKA</title>
+  <meta charset="utf-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="/himatika_web/index.php">HIMATIKA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="/himatika_web/index.php">Beranda</a></li>
+        <li class="nav-item"><a class="nav-link" href="/himatika_web/pages/tentang.php">Tentang</a></li>
+        <li class="nav-item"><a class="nav-link" href="/himatika_web/pages/visi_misi.php">Visi Misi</a></li>
+        <li class="nav-item"><a class="nav-link" href="/himatika_web/pages/kegiatan.php">Kegiatan</a></li>
+        <li class="nav-item"><a class="nav-link" href="/himatika_web/pages/contact.php">Kontak</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container mt-4">
