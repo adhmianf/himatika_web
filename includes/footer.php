5047a2501f23fedb7d6d7e92a070be1bd7d9e603
adhmianf <149399871+adhmianf@users.noreply.github.com>
2025-05-29 18:31:20 +0700

    footer.php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</div> <!-- penutup .container -->
+
+<footer class="bg-light text-center text-lg-start mt-5">
+  <div class="text-center p-3 bg-secondary text-white">
+    © <?= date('Y'); ?> HIMATIKA - Universitas Bhinneka PGRI Tulungagung
+  </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
