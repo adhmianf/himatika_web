c15536dbed5e1df1418ab40d5334c3e25d51d62c
adhmianf <149399871+adhmianf@users.noreply.github.com>
2025-05-29 18:33:55 +0700

    tentang.php

---
+++ pages/tentang.php
@@ -0,0 +1,4 @@
+<?php include '../includes/header.php'; ?>
+  <h2>Tentang HIMATIKA</h2>
+  <p>HIMATIKA adalah organisasi mahasiswa jurusan Informatika yang bergerak di bidang akademik dan pengembangan minat bakat.</p>
+<?php include '../includes/footer.php'; ?>
